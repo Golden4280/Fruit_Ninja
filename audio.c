c0c02bb847b64e8db042abb5b59a1454dbe57891
Golden4280 <mousavinica@gmail.com>
2026-03-28 17:40:08 +0000

    audio

--- audio.c
+++ audio.c
@@ -14,11 +14,12 @@
 #include "watermelons.h"
 
 #define SWITCH_BASE 0xFF200040
+#define LED_BASE    0xFF200000
 #define AUDIO_BASE  0xFF203040
 
 // Sound data arrays
-const short *miss_sound = gameovers;
-const int miss_sound_len = sizeof(gameovers) / sizeof(gameovers[0]);
+const short *gameovers_sound = gameovers;
+const int gameovers_sound_len = sizeof(gameovers) / sizeof(gameovers[0]);
 
 const short *apple_sound = Red_Apples;
 const int apple_sound_len = sizeof(Red_Apples) / sizeof(Red_Apples[0]);
@@ -44,7 +45,6 @@ const int pomegranate_sound_len = sizeof(pomegranates) / sizeof(pomegranates[0])
 const short *bomb_sound = bombs;
 const int bomb_sound_len = sizeof(bombs) / sizeof(bombs[0]);
 
-// Extra sounds if you want them later
 const short *start_sound = starts;
 const int start_sound_len = sizeof(starts) / sizeof(starts[0]);
 
@@ -54,10 +54,18 @@ const int play_sound_len = sizeof(plays) / sizeof(plays[0]);
 const short *slice_sound = slice;
 const int slice_sound_len = sizeof(slice) / sizeof(slice[0]);
 
+// Small delay
+void delay(void) {
+    volatile int i;
+    for (i = 0; i < 500000; i++);
+}
+
 // Reset and enable audio device
 void audio_init(volatile int *audio_ptr) {
     *audio_ptr = 0x8;
-    *audio_ptr = 0x0;
+    delay();
+    *audio_ptr = 0x2;
+    delay();
 }
 
 // Write one sample to both left and right channels
@@ -83,15 +91,17 @@ void play_sound(volatile int *audio_ptr, const short sound[], int length) {
     }
 }
 
-// Small delay
-void delay(void) {
-    volatile int i;
-    for (i = 0; i < 500000; i++);
+void blink_led(volatile int *led_ptr, int pattern) {
+    *led_ptr = pattern;
+    delay();
+    *led_ptr = 0x0;
+    delay();
 }
 
 int main(void) {
     volatile int *switch_ptr = (int *)SWITCH_BASE;
-    volatile int *audio_ptr = (int *)AUDIO_BASE;
+    volatile int *led_ptr    = (int *)LED_BASE;
+    volatile int *audio_ptr  = (int *)AUDIO_BASE;
 
     audio_init(audio_ptr);
 
@@ -99,30 +109,39 @@ int main(void) {
         int sw = *switch_ptr & 0x1FF;
 
         if (sw & 0x001) {
-            play_sound(audio_ptr, miss_sound, miss_sound_len);
+            blink_led(led_ptr, 0x001);
+            play_sound(audio_ptr, slice_sound, slice_sound_len);
             delay();
         } else if (sw & 0x002) {
+            blink_led(led_ptr, 0x002);
             play_sound(audio_ptr, apple_sound, apple_sound_len);
             delay();
         } else if (sw & 0x004) {
+            blink_led(led_ptr, 0x004);
             play_sound(audio_ptr, lemon_sound, lemon_sound_len);
             delay();
         } else if (sw & 0x008) {
+            blink_led(led_ptr, 0x008);
             play_sound(audio_ptr, banana_sound, banana_sound_len);
             delay();
         } else if (sw & 0x010) {
+            blink_led(led_ptr, 0x010);
             play_sound(audio_ptr, pineapple_sound, pineapple_sound_len);
             delay();
         } else if (sw & 0x020) {
+            blink_led(led_ptr, 0x020);
             play_sound(audio_ptr, watermelon_sound, watermelon_sound_len);
             delay();
         } else if (sw & 0x040) {
+            blink_led(led_ptr, 0x040);
             play_sound(audio_ptr, strawberry_sound, strawberry_sound_len);
             delay();
         } else if (sw & 0x080) {
+            blink_led(led_ptr, 0x080);
             play_sound(audio_ptr, pomegranate_sound, pomegranate_sound_len);
             delay();
         } else if (sw & 0x100) {
+            blink_led(led_ptr, 0x100);
             play_sound(audio_ptr, bomb_sound, bomb_sound_len);
             delay();
         }
